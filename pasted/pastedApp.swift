74b1a9e76ccb0285717be377eb4b24d18050ffe8
Meng Huang <mark.huang1212@gmail.com>
2021-12-10 11:44:34 +0800

    Initial Commit

---
+++ pasted/pastedApp.swift
@@ -0,0 +1,17 @@
+//
+//  pastedApp.swift
+//  pasted
+//
+//  Created by Meng on 10/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct pastedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
